19f6ef4fafee4d5bc75713cf9fe5234dc1f02622
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-04-10 22:24:12 +0200

    update

--- models/Topic.model.js
+++ models/Topic.model.js
@@ -10,6 +10,11 @@ const topicSchema = new Schema(
       type: String,
       required: true,
     },
+    categoryTopic: {
+      type: String,
+      required: true,
+      enum: ['イベント', '新商品', 'コラム'],
+    },
     description: {
       type: String,
       required: true,
