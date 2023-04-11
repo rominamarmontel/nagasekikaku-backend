c379a8bae9f80bd7526d292fca172cd9b241fbde
rominamarmontel <69083631+rominamarmontel@users.noreply.github.com>
2023-04-11 11:52:15 +0200

    index modified

--- config/index.js
+++ config/index.js
@@ -24,10 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: FRONTEND_URL,
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-      credentials: true,
-      allowedHeaders: ['Content-Type', 'Authorization'],
+      origin: [FRONTEND_URL],
     })
   )
 
